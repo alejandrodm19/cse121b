ab5c850aa9f5580d5168eabc2063ee843ce878d0
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2023-09-16 11:47:43 -0400

    Debugg Activity

--- debugging.js
+++ debugging.js
@@ -1,6 +1,13 @@
 const PI = 3.14;
-const radius = 3;
+// let radius = 3;
 let area = 0;
-area = radius * radius * pi;
-radius = 4;
-area = radius * radius * pi;
+function circleArea(radius) {
+    const area = radius * radius * PI
+    return area
+}
+
+area = circleArea(3)
+console.log(`Area 1: ${area}`)
+
+area = circleArea(4);
+console.log(`Area 2: ${area}`)
